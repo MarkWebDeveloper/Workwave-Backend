16b4f6e59c4841f46af741705320f7b170dd92d3
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-02-22 14:41:41 +0100

    tests are modified

--- src/test/java/dev/group24/workwave/announcement/AnnouncementRepositoryTest.java
+++ src/test/java/dev/group24/workwave/announcement/AnnouncementRepositoryTest.java
@@ -1,14 +1,8 @@
 package dev.group24.workwave.announcement;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
@@ -31,7 +25,7 @@ public class AnnouncementRepositoryTest {
     @DisplayName("Find all announcements")
     void testGetAllAnnouncements() {
         List<Announcement> announcements = repository.findAll();
-        assertEquals(4, announcements.size());
+        assertEquals(6, announcements.size());
         assertThat(announcements.get(0).getJobName()).isEqualTo("Programador muy Junior");
     }
 
@@ -41,23 +35,6 @@ public class AnnouncementRepositoryTest {
         Announcement anun1 = repository.findById(2L).orElseThrow();
         assertEquals(2L, anun1.getId());
         assertEquals("Madrid", anun1.getProvince());
-    }
-
-    @Test
-    void testDeleteAnnouncementById() {
-        Date date = new GregorianCalendar(2024, Calendar.FEBRUARY, 22).getTime();
-
-        Announcement anun1 = new Announcement("Islas Canarias", "Limpiador de ratones", "LimpiezaBoom", "Saber limpiar ratones", "Trabajo de tus sueños", 100L, date);
-
-        entityManager.persist(anun1);
-
-        repository.deleteById(1L);
-
-        List<Announcement> announcements = repository.findAll();
-        assertThat(announcements.size(), is(4));
-        assertThat(announcements.get(0).getJobName(), containsString("Programador sin conocimientos previos"));
-        assertThat(announcements.get(1).getJobName(), containsString("Especialista en todo"));
-        assertThat(announcements.get(3).getJobName(), containsString("Limpiador de ratones"));
     }
 
     @AfterEach
