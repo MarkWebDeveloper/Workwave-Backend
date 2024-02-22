7bccb7dd5f81b4a79a2b1116c8cd4dcbb99468ee
indiakka indiakka <78439130+indiakka@user.noreply.git>
2024-02-22 15:42:40 +0100

    prueba

--- src/main/java/dev/group24/workwave/announcement/AnnouncementRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.group24.workwave.announcement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnnouncementRepository extends JpaRepository<Announcement,Long> {
-
-}
--- src/main/java/dev/group24/workwave/interfaces/IGenericGetService.java
+++
@@ -1,8 +0,0 @@
-package dev.group24.workwave.interfaces;
-
-import java.util.List;
-
-public interface IGenericGetService<T> {
-    public List<T> getAll();
-    public T getById(Long id) throws Exception;
-}
